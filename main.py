44516ef70166a11fd1d09b52b11e36e8862b6c3c
flow <2389004553@qq.com>
2024-12-21 18:39:11 +0800

    ✨ feat(bot): 修正不能执行place的问题
    方法：在进行tof距离判定前，稍微抬高机械臂

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import progress
 import cv2
 from yolov5 import YOLOv5
-import keyboard
+import time
+from robomaster import robot, led
 
-mod_path = 'Demo/runs/train/exp9_ok/weights/best.pt'
+mod_path = 'F:/X_code/bot/robot-final-work24/best.pt'
 Yolo_v5 = YOLOv5(mod_path)
+ep_robot = robot.Robot()
+ep_robot.initialize('ap')
+ep_camera = ep_robot.camera
+ep_camera.start_video_stream(display=False)
+result = ''
 
 def detect(cv2img, show_results = False):
     __img__ = cv2.cvtColor(cv2img, cv2.COLOR_BGR2RGB)
@@ -25,9 +31,10 @@ def detect(cv2img, show_results = False):
 
     return __boxes__, __scores__, __categories__, __names__
 
-def fruit(result):
-    while not keyboard.is_pressed('esc'):
-        img = progress.get_camera().read_cv2_image(strategy = "newest", timeout = 0.5)
+def fruit():
+    count = 0
+    while count < 10:
+        img = ep_camera.read_cv2_image(strategy = "newest", timeout = 0.5)
         boxes, scores, categories, names = detect(img, show_results = False)
         flag = False
         for index, item in enumerate(boxes):
@@ -36,30 +43,57 @@ def fruit(result):
             x1, y1, x2, y2 = list(map(int, pts))
             print(names[t_class], x1, y1, x2, y2)
             if names[t_class] == result:
-                progress.seek(pos = x1, speed = 30)
+                tmp_y, tmp_x = progress.seek(chassis = ep_robot.chassis, pos = x1, speed = 5, target = 580, kx = 4000, ky = 3000, max_speed = 0.15)
+                if abs(tmp_y) <= 0.05 and abs(tmp_x) <= 0.05:
+                    count += 1
+                else:
+                    count = 0
                 flag = True
             cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
-            cv2.putText(img, names[t_class], (x1, y1), cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=2, color=(0, 0, 0),
-                        thickness=1)
+            cv2.putText(img, names[t_class], (x1, y1), cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=2, color=(0, 0, 0), thickness=1)
+            # if flag:
+            #     break
         cv2.imshow("EPCamera", img)
         if not flag:
-            progress.seek(pos = 0, speed = 30)
+            progress.seek(chassis = ep_robot.chassis, pos = 0, speed = 5, target = 580, kx = 4000, ky = 3000, max_speed = 0.15)
         cv2.waitKey(1)
+    ep_robot.chassis.drive_speed(x = 0, y = 0, z = 0)
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    progress.setup(kp = 30, ki = 10, kd = 50)
-    fruit(progress.get_result())
-    # progress.move(target_color = 'blue', base_speed = 70, start_angle = 180)
-    # progress.recognize()
-    # img = progress.get_image()
-    # boxes, scores, categories, names = detect(img, show_results=False)
-    # progress.seek(boxes, scores, categories, names, speed = 30)
-    # for i in range(2):
-    #     progress.move(base_speed = 20, start_angle = 0)
-    #     progress.seek()
-    #     progress.grab()
-    #     progress.move(base_speed = 20, start_angle = 0)
-    #     progress.place()
-    # while not keyboard.is_pressed('esc'):
-    #     pass
-    progress.end()
+    ep_arm = ep_robot.robotic_arm
+    ep_arm.recenter().wait_for_completed()
+    ep_robot.led.set_led(comp = led.COMP_ALL, r = 255, g = 255, b = 255, effect = led.EFFECT_ON)
+    pid = progress.setup(ep_robot, kp = 130, ki = 5, kd = 20)
+
+    ep_robot.gripper.open()
+    time.sleep(1)
+    result = progress.recognize(camera = ep_robot.camera)
+    # result = "banana"
+    ep_robot.led.set_led(comp = led.COMP_ALL, r = 133, g = 24, b = 247, effect = led.EFFECT_ON)
+
+    print("first lap begin")
+    progress.move(arm=ep_arm,chassis=ep_robot.chassis, camera=ep_robot.camera, vision=ep_robot.vision, pid_ctrl=pid, target_color='red', base_speed=55, start_angle=190)
+    fruit()
+    progress.grab(arm = ep_arm, gripper = ep_robot.gripper, chassis = ep_robot.chassis)
+
+    progress.move(arm=ep_arm,chassis=ep_robot.chassis, camera=ep_robot.camera, vision=ep_robot.vision, pid_ctrl=pid, target_color='red', base_speed=50, start_angle=190)
+    time.sleep(1)
+    progress.place(arm = ep_robot.robotic_arm, gripper = ep_robot.gripper, chassis= ep_robot.chassis)
+    print("first lap finished")
+
+    time.sleep(1)
+
+    print("second lap begin")
+    progress.move(arm=ep_arm,chassis=ep_robot.chassis, camera=ep_robot.camera, vision=ep_robot.vision, pid_ctrl=pid, target_color='red', base_speed=55, start_angle=190)
+    fruit()
+    progress.grab(arm = ep_arm, gripper = ep_robot.gripper, chassis = ep_robot.chassis)
+    progress.move(arm=ep_arm,chassis=ep_robot.chassis, camera=ep_robot.camera, vision=ep_robot.vision, pid_ctrl=pid, target_color='red', base_speed=50, start_angle=190)
+    time.sleep(1)
+    ep_robot.gripper.close()
+    progress.place(arm = ep_robot.robotic_arm, gripper = ep_robot.gripper, chassis= ep_robot.chassis)
+    print("second lap finished")
+
+    time.sleep(1)
+
+    del ep_robot
--- tof.py
+++ tof.py
@@ -7,10 +7,8 @@ def sub_data_handler(sub_info):
     distance = sub_info
     #print("tof1:{0}  tof2:{1}  tof3:{2}  tof4:{3}".format(distance[0], distance[1], distance[2], distance[3]))
 
-def setup():
+def setup(ep_robot):
     global ep_sensor
-    ep_robot = robot.Robot()
-    ep_robot.initialize('ap')
     ep_sensor = ep_robot.sensor
     ep_sensor.sub_distance(freq = 5, callback = sub_data_handler)
 
